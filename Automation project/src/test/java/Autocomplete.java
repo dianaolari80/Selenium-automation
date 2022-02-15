aef3dae397becf6ad5448f9f41aaede961bec1e6
dianaolari80 <diana_balc@yahoo.com>
2022-02-15 21:31:27 +0200

    Initial commit

---
+++ Automation project/src/test/java/Autocomplete.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Autocomplete {
+    public static void main(String [] arg){
+            System.setProperty("webdriver.chrome.drive","C:/Users/dianaolari/Documents/chromedriver.exe");
+
+            //la linia 6 i-am spus sistemului unde sa gaseasca driverul
+        WebDriver driver=new ChromeDriver();//aici am creat un nou obiect al clasei Chrome driver care se numeste driver si care are tipul de data ChromeDriver
+    /* o variabila este o adresa de memorie la care sunt stocate informatiile
+        o functie reprezinta un set de instructiuni grupate sub un singur nume
+        o clasa reprezinta un tipar care defineste atributele pe care trebuie sa le aiba un obiect si actiunile pe care poate sa le faca
+        un obiect este o instanta a unei clase adica o reprezentare reala creata pe baza tiparului definit in clasa
+        un tip de data este o proprietate a unei variabile sau a unei functii care specifica ce informatii pot sa fie stocate in variabila sau returnate de catre functie
+        putem spune ca o functie returneaza ceva atunci cand trimite rezultatul actiunii pe care o face catre exterior
+     */
+    }
+
+
+}
